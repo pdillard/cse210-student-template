3fc3852f9581dcfeb87b90830ab0a3facef71c8d
pdillard <91156003+pdillard@users.noreply.github.com>
2025-04-23 21:08:36 -0600

    Update Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,23 @@
 using System;
 
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Program to compute area of a circle
+
+        // Get radius of the circle from the user.
+        Console.Write("Please enter the radius: ");
+        string text = Console.ReadLine();
+        double radius = double.Parse(text);
+
+        // Compute the area of the circle.
+        double area = Math.PI * radius * radius;
+
+
+        // Display the area for the user to see.
+        Console.WriteLine($"Area of the circle: {area}");
     }
 }
