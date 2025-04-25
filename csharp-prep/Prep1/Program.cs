bdc6c6f0670c04306d40e7dcbb18554c32bfccf2
pdillard <91156003+pdillard@users.noreply.github.com>
2025-04-25 14:34:53 -0600

    Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+      
+      // Prompt the User for First Name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+      // Prompt the User for Last Name  
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
     }
 }
