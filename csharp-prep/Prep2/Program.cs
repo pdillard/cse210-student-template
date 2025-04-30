384d725d7b3abc6efcabcd3b76f0bcd24df3a54a
pdillard <91156003+pdillard@users.noreply.github.com>
2025-04-30 11:52:30 -0600

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90) {
+            Console.WriteLine("Your grade is A");
+        } 
+        else if (grade >= 80 ){
+            Console.WriteLine("Your grade is B");
+        }
+        else if (grade >= 70 ) {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (grade >= 60 ) {
+            Console.WriteLine("Your grade is D");
+        }
+        else {
+            Console.WriteLine("Your grade is F");
+        }
     }
 }
