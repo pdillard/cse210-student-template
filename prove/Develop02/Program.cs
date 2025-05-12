dd853d37043aa7dfa8053f3f87211c0695fa5bd7
pdillard <91156003+pdillard@users.noreply.github.com>
2025-05-12 09:41:17 -0600

    Update Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+
+        bool running = true;
+        while (running != true)
+
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == 5) {
+                running = false;
+            }
+            else if (userInput == 1) {
+                // writeJournalEntry()
+                return;
+            }
+            else if (userInput == 2) {
+                // displayJournalEntry()
+                return;
+            }
+            else if (userInput == 3) {
+                // loadJournal()
+                return;
+            }
+            else if (userInput == 4) {
+                // saveJournal()
+                return;
+            }
+            else {
+                Console.WriteLine("Invalid input");
+            }
+
+
+
+        
+
+
     }
 }
